861b57695c3bc45ee3164a301abb8d0549677c2f
kallinnas <kallinnas@gmail.com>
2021-08-20 12:11:12 +0100

    add devops exercise

---
+++ ex.py
@@ -0,0 +1,63 @@
+import random
+from datetime import date, datetime
+
+class Ex:
+    def call():
+        list = ['I', 'kk']
+        for w in list:
+            print(w.title())
+
+    def replaceChar():
+        print(('laptop'.replace('l', 'k')).upper())
+
+    def revArr():
+        arr = []
+        arr1 = []
+        for num in range(10):
+            arr.append(random.randint(1, 100))
+        # arr.sort()
+        # print(arr)
+        arr.sort(reverse=True)
+        print(arr)
+
+    def newPass():
+        password = '!@#$%^&*)1234567890qwertyuiopasdfghjklzxcv'
+        len_pass = int(input("length: "))
+        a = "".join(random.sample(password, len_pass))
+        print(f"password {a}")
+
+    def sumOfNums():
+        num = str(input("enter the number:"))
+        sum = 0
+        for n in num:
+            sum += int(n)
+        print(sum)
+
+    def firstCharCapLetter():
+        countries = ['kiyv', 'jerusalem', 'Kfar Saba']
+        for c in countries:
+            print(c.capitalize())
+
+    def countCapLetters():
+        line = str(input("enter the line with caps & low letters:"))
+        count = 0
+        for word in line:
+            for char in word:
+                if char.isupper():
+                    count += 1
+        print(count)
+
+    def currentDateTime():
+        print("Today's date:", date.today())
+        now = datetime.now()
+        dt_string = now.strftime("%H:%M:%S")
+        print("The time is:", dt_string)
+
+
+Ex.currentDateTime()
+# Ex.countCapLetters()
+# Ex.newPass()
+# Ex.firstCharCapLetter()
+# Ex.revArr()
+# Ex.replaceChar()
+
